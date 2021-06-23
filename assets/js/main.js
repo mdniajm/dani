e0856b1cac6f56e370c245a15458d530f5e782b5
mdniajm <60269882+mdniajm@users.noreply.github.com>
2021-06-23 21:14:28 +0600

    Add files via upload
    done

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+(function ($) {
+"use strict";
+/*--------------------------------------
+	Mean Menu Active
+----------------------------------------*/
+$('header .main-menu').meanmenu({
+	meanMenuContainer: '.header-area',
+	meanScreenWidth: 2000,
+});
+   
+})(jQuery);	
+
+
+new WOW().init();
